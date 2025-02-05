2c771e3063c24e74528e944370e6ef9ffce478f1
eunseo <92281453+les0498@users.noreply.github.com>
2025-02-05 23:17:04 +0900

    setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Routes, Route, Link, useNavigate } from "react-router-dom"; 
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Notfound from './pages/Notfound';
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지 
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
 function App() {
-  const [count, setCount] = useState(0)
+  const nav = useNavigate();
 
-  return (
+  const onClickButton = () => {
+    nav("/new");
+  }
+
+  return  (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/new"}>New</Link>
+      <Link to={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onClickButton}> New 페이지로 이동 </button>
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/new" element={<New />} /> 
+      <Route path="/diary/:id" element={<Diary />} /> 
+      <Route path="*" element={<Notfound />} />
+    </Routes>
     </>
-  )
+  ); 
 }
 
 export default App
